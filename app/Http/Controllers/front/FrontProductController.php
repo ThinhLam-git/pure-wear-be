6e78fca7145d1f380caddde10ba9196842f88ba6
Tuấn Thịnh <ltthinh111@gmail.com>
2025-08-16 10:19:44 +0700

    Add FrontProductController with product, category, and brand retrieval methods; update API routes

---
+++ app/Http/Controllers/front/FrontProductController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\front;
+
+use App\Http\Controllers\Controller;
+use App\Models\Brand;
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class FrontProductController extends Controller
+{
+    //
+    public function getProducts(Request $request)
+    {
+        $products = Product::orderBy('created_at', 'desc')
+            ->where('status', 1);
+
+        if(!empty($request->category_id)){
+            $catArray = explode(',', $request->category_id);
+            $products->whereIn('category', $catArray);
+        }
+
+        if(!empty($request->brand_id)){
+            $brandArray = explode(',', $request->brand_id);
+            $products->whereIn('brand', $brandArray);
+        }
+
+        $products = $products->get();
+        return response()->json([
+            'status' => 200,
+            'message' => 'Retrieved Products successfully',
+            'data' => $products,
+        ]);
+    }
+
+    public function latestProducts()
+    {
+        $products = Product::orderBy('created_at', 'desc')
+            ->where('status', 1)
+            ->limit(8)
+            ->get();
+        return response()->json([
+            'status' => 200,
+            'message' => 'Retrieved Latest Products successfully',
+            'data' => $products,
+        ]);
+    }
+
+    public function featuredProducts()
+    {
+        $products = Product::orderBy('created_at', 'desc')
+            ->where('status', 1)
+            ->where('is_featured', 'yes')
+            ->limit(4)
+            ->get();
+        return response()->json([
+            'status' => 200,
+            'message' => 'Retrieved Featured Products successfully',
+            'data' => $products,
+        ]);
+    }
+
+    public function getCategories()
+    {
+        $categories = Category::orderBy('created_at', 'desc')->where('status', 1)->get();
+        return response()->json([
+            'status' => 200,
+            'message' => 'Retrieved Categories successfully',
+            'data' => $categories,
+        ]);
+    }
+
+    public function getBrands()
+    {
+        $brands = Brand::orderBy('created_at', 'desc')->where('status', 1)->get();
+        return response()->json([
+            'status' => 200,
+            'message' => 'Retrieved Brands successfully',
+            'data' => $brands,
+        ]);
+    }
+}
